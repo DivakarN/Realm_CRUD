34f6a4c0851c612eda6d9b18d4e094b0be213db9
Vijay <Sagayanavis.vijay@tpfsoftware.com>
2019-12-14 10:45:09 +0530

    Some uncommitted Changes

--- app/src/main/java/com/sysaxiom/realmcrud/RealmConfig.kt
+++ app/src/main/java/com/sysaxiom/realmcrud/RealmConfig.kt
@@ -5,7 +5,7 @@ import io.realm.Realm
 import io.realm.RealmConfiguration
 
 
-class MyApplication : Application() {
+    class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         // Initialize Realm. Should only be done once when the application starts.
